571fbc8f6fe23de346c368cf8b5476736ed16f77
LuisRoneii <luisroninho@gmail.com>
2026-08-23 15:33:04 -0300

    chore: cria estrutura inicial do app.js

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// Seleção dos elementos do DOM
+const form = document.getElementById('task-form');
+const input = document.getElementById('task-input');
+const list = document.getElementById('task-list');
+const emptyMessage = document.getElementById('empty-message');
+
+// Array que vai armazenar as tarefas
+let tasks = [];
